47e3fa8fc05db6d6201098e4641de3495ca5a787
isobelbutler <isobutler@icloud.com>
2023-02-20 15:22:35 +0000

    yay!! cracked the rpt cards & accessing object

--- js/script.js
+++ js/script.js
@@ -33,32 +33,36 @@ let movieData = {
       cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
     },
   };
-  
+ 
+// 1. CREATES THE REPEATED CARDS
 
+let html = '';
 
-for (var [title, data] of Object.entries(movieData)) {
-console.log (title);
-// console.log (movieData.movieTitle);
-
+for (let i = 0; i < Object.keys(movieData).length; i++) {
+  let card = Object.values(movieData)[i]; // BREAKTHROUGH!!!! - Values lets you access the data of each title.
+  let title = Object.keys(movieData)[i]; // Keys lets you access the the actual title. So it goes object > keys > values.
+  html +=
+    `<div class="card">
+    <!-- THIS WILL BE REPLACED WITH JAVASCRIPT -->
+    <img src="images/filler.jpg" alt="#">
+    <h4>${title}</h4>
+    <p>${card.rating}/10</p>
+    <p>
+      Summary: ${card.plot}
+    </p>
+    <p>Starring: ${card.cast.join(', ')}.</p> 
+      <!-- 
+        Use .join to add the comma and space. Also: I found this code but don't 
+        understand it yet, when added it will replace 
+        last comma with ', and' =
+         ...join(', '))}.replace(/, ([^,]*)$/, ' and $1') 
+        Come back and pick this apart if time.
+        -->
+    <p>First aired: ${card.year}.</p>
+    <p>Runtime: ${card.runtime} minutes.</p>
+  </div>
+    `
+    ;  
 }
 
-for ( let title in movieData ) {
-  console.log( `Film Title: ${title}` );
-  console.log( `Plot: ${movieData[title].plot}` );
-  console.log( `Starring: ${movieData[title].cast.join(', ')}`);
-  
-
-
-  // console.log(Object.values([prop]));
-}
-
-
-
-
-// for ( let prop in movieData ) {
-//   console.log(`Plot: ${movieData[prop].plot}`);
-// }
-
-// for ( let prop in movieData ) {
-//   console.log(movieData[prop]);
-// }
+document.querySelector('.card-container').insertAdjacentHTML('beforeend', html); 
